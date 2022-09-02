7560046e1364ac6fb790b5c21f0b91989dc43dd8
Samuel <olivsamuk@gmail.com>
2022-09-02 11:50:04 -0300

    02-09

--- plot_papers_per_year_radar.py
+++ plot_papers_per_year_radar.py
@@ -13,33 +13,55 @@ plt.rc('ytick', labelsize=18)    # fontsize of the tick labels
 plt.rc('legend', fontsize=18)    # legend fontsize
 plt.rc('figure', titlesize=18)  # fontsize of the figure title
 
-def get_data(csv_file):
+def get_data(csv_file, period):
     papers = pd.read_csv(csv_file)
-    data = {}
+    data = {'attack model': 0, 'attack detection and mitigation': 0, 'diagnosabilty': 0, 'state estimation': 0, 'supervisor synthesis': 0, 'system recovery': 0}
     for index, each_paper in papers.iterrows():
-        if data.get(each_paper['attack']):
-            data[each_paper['attack']] += 1
-        else:
-            data[each_paper['attack']] = 1
+        if period == 1:
+            if each_paper['year'] >= 2006 and each_paper['year'] <= 2016:
+                data[each_paper['problem']] += 1
+        elif period == 2:
+            if each_paper['year'] >= 2017 and each_paper['year'] <= 2020:
+                data[each_paper['problem']] += 1
+        elif period == 3:
+            if each_paper['year'] >= 2021:
+                print(each_paper['title'])
+                data[each_paper['problem']] += 1
+
     return data
 
-papers = get_data('papers-categorized-new.csv')
+papers_p1 = get_data('papers-categorized_active.csv',1)
+papers_p2 = get_data('papers-categorized_active.csv',2)
+papers_p3 = get_data('papers-categorized_active.csv',3)
+print(papers_p1)
 
 
-categories = list(papers.keys())
+# categories = list(papers.keys())
+categories = ['attack detection and mitigation', 'attack model', 'supervisor synthesis', 'diagnosabilty', 'state estimation', 'system recovery']
 categories = [*categories, categories[0]]
 
-restaurant_1 = list(papers.values())
-
+restaurant_1 = list(papers_p1.values())
 restaurant_1 = [*restaurant_1, restaurant_1[0]]
 
+restaurant_2 = list(papers_p2.values())
+restaurant_2 = [*restaurant_2, restaurant_2[0]]
+
+restaurant_3 = list(papers_p3.values())
+restaurant_3 = [*restaurant_3, restaurant_3[0]]
 
 label_loc = np.linspace(start=0, stop=2 * np.pi, num=len(restaurant_1))
-print(label_loc)
+
 plt.figure(figsize=(8, 8))
 plt.subplot(polar=True)
-plt.plot(label_loc, restaurant_1)
+plt.plot(label_loc, restaurant_1, linewidth=1, linestyle='solid', color='#797D7F')
+plt.fill(label_loc, restaurant_1, '#CACFD2', alpha=.5, zorder=5)
+plt.plot(label_loc, restaurant_2, linewidth=1, linestyle='solid', color='#154360')
+plt.fill(label_loc, restaurant_2, '#5499C7', alpha=.5, zorder=1)
+plt.plot(label_loc, restaurant_3,linewidth=1, linestyle='solid', color='#7B241C')
+plt.fill(label_loc, restaurant_3, '#D98880', alpha=.5, zorder=0)
 
 lines, labels = plt.thetagrids(np.degrees(label_loc), labels=categories)
 plt.legend()
 plt.show()
+
+
