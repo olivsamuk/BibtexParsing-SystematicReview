ee45c38bee6e01768bb3b6b5b0451b44701c622c
Samuel <olivsamuk@gmail.com>
2022-11-03 15:44:30 -0300

    new

--- plot_papers_per_attack_type.py
+++ plot_papers_per_attack_type.py
@@ -48,7 +48,7 @@ print(papers)
 
 names = change_names(list(papers.keys()))
 amount = list(papers.values())
-
+print(names)
 
 # # Create a circle at the center of the plot
 # my_circle = plt.Circle((0, 0), 0.7, color='white')
@@ -71,11 +71,14 @@ size = 0.3
 
 
 ax.pie([papers['actuator'], papers['sensor'], papers['sensor-actuator'], papers['denail of service'],103],
+       labels=['Actuator attacks', 'Sensor attacks', 'Sensor-actuator attacks', 'Denail of Service (DoS)', 'Passive attacks (Eavesdropping)'],
         colors=['#6495ED', '#CCCCFF', '#DE3163', '#9FE2BF', '#FFFFFF','#40E0D0', '#FFBF00', '#FF7F50', '#7A1B9D'],
-       autopct='%1.2f%%', pctdistance=0.8,
+       # autopct='%1.2f%%',
+       pctdistance=0.8,
        radius=1, wedgeprops=dict(width=size, edgecolor='w', linewidth=2))
 ax.pie([papers['actuator']+papers['sensor']+papers['sensor-actuator']+papers['denail of service'], papers['passive']],
-       autopct='%1.2f%%', pctdistance=.7,
+       # autopct='%1.2f%%',
+       pctdistance=.7,
        radius=1-size, wedgeprops=dict(width=size, edgecolor='w', linewidth=2))
-plt.title('Population')
+# plt.title('Population')
 plt.show()
